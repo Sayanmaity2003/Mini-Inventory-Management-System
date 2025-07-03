201e460ecc526b1d2b5dadc53090f8fe9398c89e
Sayan Maity <sayanmaity2003@gmail.com>
2025-07-03 12:18:04 +0530

    Admin Sidebar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,55 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Root from "./utils/Root";
 import Login from "./pages/Login";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
         <Route
-          path="/admin/dashboard"
+          path="/admin-dashboard"
           element={
             <ProtectedRoutes requireRole={["admin"]}>
-              <h1>Admin Dashboard</h1>
+              <Dashboard/>
             </ProtectedRoutes>
           }
-        />
+        >
+          <Route
+          index
+          element={<h1>Summary of dashboard</h1>}
+          />
+
+          <Route
+          path="categories"
+          element={<h1>Categories</h1>}
+          />
+
+          <Route
+          path="products"
+          element={<h1>Products</h1>}
+          />
+
+          <Route
+          path="suppliers"
+          element={<h1>Suppliers</h1>}
+          />
+
+          <Route
+          path="orders"
+          element={<h1>Oders</h1>}
+          />
+
+          <Route
+          path="users"
+          element={<h1>Users</h1>}
+          />
+
+          {/* <Route
+          path="profile"
+          element={<h1>Profile</h1>}
+          /> */}
+          </Route>
         <Route
           path="/customer/dashboard"
           element={<h1>Customer Dashboard</h1>}
